2622687e6a9c85dfa8c232271a45d2f1ac3c2663
Maka2200 <margot.rajkovic@gmail.com>
2020-08-19 11:08:35 -0700

    Update GUI.java

--- src/edu/rpi/cs/csci4963/su20/dzm/pacman/GUI.java
+++ src/edu/rpi/cs/csci4963/su20/dzm/pacman/GUI.java
@@ -21,34 +21,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class Pacman extends JFrame {
-
-    public Pacman() {
-        
-        initUI();
-    }
-    
-    private void initUI() {
-        
-        add(new GUI());
-        
-        setTitle("Pacman");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(380, 420);
-        setLocationRelativeTo(null);
-    }
-
-    public static void main(String[] args) {
-
-        EventQueue.invokeLater(() -> {
-            var ex = new Pacman();
-            ex.setVisible(true);
-        });
-    }
-}
-
-
-
 public class GUI extends JPanel implements ActionListener {
 
     private Dimension d;
